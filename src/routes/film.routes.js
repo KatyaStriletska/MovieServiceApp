c8aacc6e2de5a61520f0fc0f462e88f041903ecb
Striletska Kateryna <striletskay2005@gmail.com>
2025-04-10 00:03:30 +0300

    init

--- src/routes/film.routes.js
+++ src/routes/film.routes.js
@@ -7,7 +7,7 @@ router.get('/movies', MovieController.findMany); // get all movies
 router.get('/movie/:id', MovieController.findByID); // get one movie by id
 
 router.post('/movie', MovieController.create); // create new movie
-router.post('/movie/:id', MovieController.update); // update movie by id
+router.put('/movie/:id', MovieController.update); // update movie by id
 router.delete('/movie/:id', MovieController.delete); // delete movie by id
 
 module.exports = router;
